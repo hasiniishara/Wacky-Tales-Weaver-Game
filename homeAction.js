70d425616e63fb3c9259ffdb1983e998b8bf7abe
HasiniIshara <hasiniisharao4@gmail.com>
2024-03-01 17:11:53 +0200

    Update Code.

---
+++ homeAction.js
@@ -0,0 +1,19 @@
+function viewStory(image){
+    switch(image) {
+        case 'img1' : 
+            document.getElementById("c1").innerText = `\tOnce upon a time, there was a kingdom ruled by a humble King Charles and Queen Mary. Fairies gave the family of the King magical powers. Sometime later, the royal family was blessed with a beautiful baby girl. 
+                                                        \nOn the day of the little girl’s birth, the god fairy visited her and blessed her with the powers of a fairy. To this, Queen Mary and King Charles rejoiced, and the queen said, "I am so lucky to be the mother of a fairy princess". The royal parents gave their daughter the name Jasmine.`;
+            break;
+        case 'img2' :
+            document.getElementById("c2").innerText = `\tThe years passed by. Soon the little princess grew into a gorgeous young fairy princess. One day, Princess Jasmine disappeared from her palace without any clue. The King and Queen got frantic and shattered. The Queen stopped eating and got sick because she was totally heartbroken in the absence of her daughter.
+                                                        \nTwo days later, Haider, a young magician, visited The King’s court and offered his help in finding the princess. “Your Highness, I believe I can be a great help to you in finding Princess Jasmine. Allow me to search for her”, he requested the King.`;
+            break;
+        case 'img3' :
+            document.getElementById("c3").innerText = `\tHaider, the young magician, has a magical mirror which used to show the whereabouts of a person. When he searched for the princess in the mirror, he saw that Princess Jasmine had been taken away by the cruel wizard Jafar.“Your Highness, the princess has been abducted by Jafar, the wizard. Please do not worry. I will soon find her and bring her home”, he reassured the King.`;
+            break;
+        case 'img4' :
+            document.getElementById("c4").innerText = `\tSoon, Haider found the location of the cruel wizard’s castle and set out to save the princess. He entered the castle secretly. “Abracadabra, Abracadabra, turn into a cat”, he turned the Jafar into a cat using his magic wand and spell. He rescued the princess immediately. Princess Jasmine fell in love with him at first sight and thanked the magician.
+                                                        \nAfter that, Princess Jasmine and Haider married and lived happily ever after.`;
+            break;
+    }
+}
